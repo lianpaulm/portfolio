6ac805fe8b8e7a909a1d05a644812ca4718861cf
Lian Paul <lianpaulmolo08@gmail.com>
2022-06-18 16:23:39 +0800

    adding styled footer

---
+++ src/components/styles/Footer.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #222;
+  margin-top: 1rem;
+  color: #f4f4f4;
+
+  p {
+    font-size: 0.9rem;
+  }
+`;
