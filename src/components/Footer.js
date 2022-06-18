aff5c7cbcc7dc98b0cae9df831d06ef8ac9398ec
Lian Paul <lianpaulmolo08@gmail.com>
2022-06-18 16:12:16 +0800

    adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledFooter } from './styles/Footer.styled';
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <p>© Copyright 2022 by Lian Paul Molo</p>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
